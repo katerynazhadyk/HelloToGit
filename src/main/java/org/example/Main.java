2b459adecb2476d90204d1a6bed471e92ec0c4b9
Kateryna <katak3848@gmail.com>
2022-12-21 18:12:29 +0100

    Added printf to run1()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        new Main().run();
+        new Main().run1();
+    }
+
+    private void run1() {
+        int nBox = 3;
+        int apples = 34;
+        System.out.printf("в коробке %d лежит %d ябл ",nBox,apples);
     }
 
     private void run() {
