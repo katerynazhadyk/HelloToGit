cdf4f6ae189c50a89113594bf1b6e888fde33643
Kateryna <katak3848@gmail.com>
2022-12-21 18:18:01 +0100

    Added printf with double to run2()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        new Main().run1();
+        new Main().run2();
+    }
+
+    private void run2() {
+        String name = "Shirt";
+        double prise = 450.50;
+        System.out.printf("товар: %s, стоит: %f грн",name,prise);
     }
 
     private void run1() {
