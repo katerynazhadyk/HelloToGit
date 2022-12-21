9928fd5014b5c9750f1862012043b0947eddfc3a
Kateryna <katak3848@gmail.com>
2022-12-21 18:06:02 +0100

    Hello

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        System.out.println("Hello world!");
+    }
+
+}
